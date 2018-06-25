c700459d02de05c16559a72eee3aa8bb6c84f966
zengkai <1965599425@qq.com>
2018-06-25 23:35:18 +0800

    添加了vue的element-ui学习

---
+++ element-ui/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="app">
+    
+    <el-button loading
+    type="success">
+      提交
+    </el-button>
+
+    <el-button 
+    icon="el-icon-edit"
+    v-on:click.native="change"
+    type="primary"
+    :loading=isLoading>
+    </el-button>
+
+    <el-button :disabled="disabled"
+    type="info">
+      提交
+    </el-button>
+
+    <el-button :disabled="disabled"
+    type="warning">
+      提交
+    </el-button>
+
+    <el-button :disabled="disabled"
+    type="danger">
+      提交
+    </el-button>
+  </div>
+</template>
+
+<script>
+import ElButton from '@/components/ElButton'
+export default {
+  name: 'App',
+  data () {
+    return {
+      isLoading: true,
+      disabled: false
+    }
+  },
+  components: {
+    'el-button': ElButton
+  },
+  mounted () {
+    setTimeout(() => {
+      this.disabled = true;
+    }, 2000);
+  },
+  methods:{
+    change:function(){
+      console.log('1111')
+      this.isLoading=!this.isLoading;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
